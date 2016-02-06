3f60414a3bf41caa47ea19c5e4c0436c89bc6afa
sameesh-s <sameesh.fisat@gmail.com>
2016-02-06 21:47:25 +0530

    authentication module

--- isRegisterd.php
+++ isRegisterd.php
@@ -1,15 +1,17 @@
 <?php
 echo "<div class='container'><div class='row'>";
 session_start();
-if(!isset($_SESSION['username']))
-	{
-	echo "<a href='Login.html'>Login</a><a href='Register.html'>&nbsp|&nbspRegister</a>";
-	}
-else
-	{
-	echo "<a href='Account.php'>";
-	echo $_SESSION['username'];
-	echo "</a><a href='Wishlist.html'>&nbsp|&nbspWishlist</a>";
-	}
+	if(!isset($_SESSION['username']))
+		{
+		echo "<a href='Login.html'>Login</a>&nbsp|&nbsp<a href='Register.html'>Register</a>";
+		}
+		else
+		{
+		echo "<a href='Account.php'>";
+		echo $_SESSION['username'];
+		echo "&nbsp|&nbsp</a><a href='Wishlist.html'>Wishlist</a>&nbsp|&nbsp";
+		echo "<a href='Logout.php'>Log out</a>";
+		}
+	session_write_close ();
 echo "</div></div>";
 ?>
