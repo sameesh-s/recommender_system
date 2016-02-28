8056cff63eab01c3c72fccf6d5730b134551e274
sameesh-s <sameesh.fisat@gmail.com>
2016-02-28 20:21:26 +0530

    total time spend on page is captured

--- product.php
+++ product.php
@@ -10,7 +10,6 @@ class Product
         {
         include "dbconnect.php";
         $this->prod_id=$param;
-        $this->generateScript();
         $query="select category_id,brand,name,image,price from product where product_id = ".$this->prod_id."";
         $rslt=mysqli_query($con,$query);
            while($row=mysqli_fetch_row($rslt)) {
@@ -31,15 +30,18 @@ class Product
                     $.ajax({url: 'addToWish.php?prod_id=".$this->prod_id."' });
                     }
                 function ck(){ 
-                    $.ajax({url: 'start.php'});
+                    $.ajax({url:'start.php'});
                     }
                 function end(){
                     $.ajax({url:'stop.php?prod_id=".$this->prod_id."'});
                     }
-                window.unload=end();
+                window.addEventListener(\"beforeunload\", function (e) { end();
+                    (e || window.event).returnValue = null ;
+                    return null;
+                    });
                 </script><body onload=ck();>";
         }
-    public function printBasic()
+    public function printWhole()
         {
             echo "<div class='container'><div class='row'>";
             echo "<div class='col-md-4'>";
@@ -64,6 +66,19 @@ class Product
                 echo "<br>Brand :".$this->brand;
                 echo "<br>price :".$this->price."<hr>";
                 echo "<br>Total :".$this->price*$quantity;
+            echo "<a href='Login.html'><i class='glyphicon glyphicon-earphone'></i>Remove </a>&nbsp|&nbsp<a href='addToWish.php?prod_id=".$this->prod_id."'>Move To Wishlist</a>";
+            echo "</div></div></div><hr>";
+        }
+    public function printBasic()
+        {
+            echo "<div class='container'><div class='row'>";
+            echo "<div class='col-md-2'>";
+                echo "<img class='img-rounded img-responsives' width='100%' src='images/".$this->image."'>";
+                echo "</div>";
+            echo "<div class='col-md-10'><h1>".$this->name."</h1>";
+                echo "<br>Category :".$this->category_id;
+                echo "<br>Brand :".$this->brand;
+                echo "<br>price :".$this->price."<hr>";
             echo "<a href='Login.html'><i class='glyphicon glyphicon-earphone'></i>Remove </a>&nbsp|&nbsp<a href='addToWish.php?prod_id=".$this->prod_id."'>Move To Wishlist</a>";
             echo "</div></div></div><hr>";
         }
